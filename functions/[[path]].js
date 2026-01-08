22977398922360bed079ed6200c1ac67d2547a31
JinxiHexi602 <63846946+JinxiHexi602@users.noreply.github.com>
2026-01-08 20:39:56 +0800

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,17 +1,5 @@
-export async function onRequest(context) {
-  const { request } = context;
+export async function onRequest({ request }) {
   const url = new URL(request.url);
-  
-  // 修改目标域名
   url.host = 'generativelanguage.googleapis.com';
-  
-  // 构造新的请求
-  const modifiedRequest = new Request(url.toString(), {
-    method: request.method,
-    headers: request.headers,
-    body: request.body,
-    redirect: 'manual'
-  });
-
-  return fetch(modifiedRequest);
+  return fetch(new Request(url.toString(), request));
 }
